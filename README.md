0d2e8e0574ad6ddff88ce3654061af0ac1057ff2
nexolit <nexolit@skiff.com>
2023-12-16 13:50:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Solaro
+A smart solar vacuum cleaner made with Microbit.
+
+This is the repo contains the code of it's website interface.
+
+## Resources
+Icons used are from [Google Fonts](https://fonts.google.com/icons), and are under [Apache 2 license](./LICENSE).
+Only the color was changed.
+
+## Installing
+Get into the root directory of the project and run:
+```npm install```
+## Running
+To host a local server run:
+```npm run dev```
---
+++ js/index.js
@@ -0,0 +1,172 @@
+// Import the necessary Three.js components
+import * as THREE from 'three'
+
+const container = document.getElementById('renderer')
+const renderer = new THREE.WebGLRenderer()
+container.appendChild(renderer.domElement)
+
+const computedStyle = getComputedStyle(container)
+const width = parseInt(computedStyle.width)
+const height = parseInt(computedStyle.height)
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(90, width / height, 0.1, 1000)
+
+scene.background = new THREE.Color(0xFFFFFF)
+renderer.setSize(width, height)
+renderer.setPixelRatio(window.devicePixelRatio)
+
+// Initialize variables for tracking mouse/touch interactions
+let isDragging = false
+let previousMouseX, previousMouseY
+let touchStartDistance
+
+const zoomSpeed = 0.003
+const minZoom = 1
+const maxZoom = 15
+let currentZoom = 10
+
+// Add event listeners for mouse and touch interactions
+renderer.domElement.addEventListener('mousedown', onMouseDown)
+renderer.domElement.addEventListener('mousemove', onMouseMove)
+renderer.domElement.addEventListener('mouseup', onMouseUp)
+
+renderer.domElement.addEventListener('touchstart', onTouchStart)
+renderer.domElement.addEventListener('touchmove', onTouchMove)
+renderer.domElement.addEventListener('touchend', onTouchEnd)
+
+renderer.domElement.addEventListener('wheel', onMouseWheel)
+
+function onMouseDown(event) {
+    renderer.domElement.style.cursor = "grabbing"
+    
+    isDragging = true
+    previousMouseX = event.clientX
+    previousMouseY = event.clientY
+}
+
+function onMouseMove(event) {
+    if (isDragging) {
+        const deltaX = ((event.clientX - previousMouseX) / 100) * currentZoom
+        const deltaY = ((event.clientY - previousMouseY) / 100) * currentZoom
+        camera.position.x -= deltaX
+        camera.position.y += deltaY // Invert Y-axis for Three.js
+        previousMouseX = event.clientX
+        previousMouseY = event.clientY
+        render()
+    }
+}
+
+function onMouseUp() {
+    renderer.domElement.style.cursor = "grab"
+    isDragging = false
+}
+
+function onMouseWheel(event) {
+    event.preventDefault()
+    currentZoom += event.deltaY * zoomSpeed
+
+    currentZoom = Math.max(minZoom, Math.min(currentZoom, maxZoom))
+
+    camera.position.z = currentZoom
+    render()
+}
+
+function onTouchStart(event) {
+    event.preventDefault()
+    isDragging = true
+    if(event.touches.length > 1) {
+        const touch1 = event.touches[0]
+        const touch2 = event.touches[1]
+        touchStartDistance = (touch1.clientX - touch2.clientX) + (touch1.clientY - touch2.clientY)
+    } else {
+        const touch = event.touches[0]
+        previousMouseX = touch.clientX
+        previousMouseY = touch.clientY
+    }
+}
+
+function onTouchMove(event) {
+    event.preventDefault()
+    if (isDragging) {
+        if(event.touches.length > 1) {
+            touchZoom(event.touches)
+        } else {
+            const touch = event.touches[0]
+            const deltaX = ((touch.clientX - previousMouseX) / 100) * currentZoom
+            const deltaY = ((touch.clientY - previousMouseY) / 100) * currentZoom
+            camera.position.x -= deltaX
+            camera.position.y += deltaY
+            previousMouseX = touch.clientX
+            previousMouseY = touch.clientY
+        }
+        render()
+    }
+}
+
+function touchZoom(touches) {
+    const touch1 = touches[0]
+    const touch2 = touches[1]
+
+    const touchCurrentDistance = (touch1.clientX - touch2.clientX) + (touch1.clientY - touch2.clientY)
+    
+    currentZoom *= touchStartDistance / touchCurrentDistance
+    currentZoom = Math.max(minZoom, Math.min(currentZoom, maxZoom))
+
+    camera.position.z = currentZoom
+
+    render()
+
+    touchStartDistance = touchCurrentDistance
+}
+
+function onTouchEnd() {
+    isDragging = false
+}
+
+// Set up the camera position and rendering
+camera.position.z = currentZoom
+
+//Fetch the data and do a for loop
+function fetchData() {
+    let points = []
+    points.push(addPoint(1, 2))
+    points.push(addPoint(1, 10))
+    points.push(addPoint(5, 8))
+    points.push(addPoint(5, 2))
+
+    const material = new THREE.MeshBasicMaterial({color: 0x00ff00})
+    const geometry = new THREE.BufferGeometry().setFromPoints(points)
+    const lines = new THREE.Line(geometry, material)
+    scene.add(lines)
+}
+
+function addPoint(x, y) {
+    //Draw an image a dot
+    return new THREE.Vector2(x, y)
+}
+
+function drawGrid() {
+    const size = 100;
+    const divisions = size;
+
+    const grid = new THREE.GridHelper(size, divisions);
+
+    grid.rotation.x = Math.PI / 2;
+    grid.position.z = 0
+
+    scene.add(grid);
+}
+
+function animate() {
+    requestAnimationFrame(animate)
+    render()
+}
+
+function render() {
+    renderer.render(scene, camera)
+}
+
+drawGrid()
+fetchData()
+animate()
