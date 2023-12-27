97e2480a386b5b720204237a9b51dbc3f594da76
nexolit <nexolit@skiff.com>
2023-12-27 20:12:46 +0100

    Added light/dark mode support and toggle functionality

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ const height = parseInt(computedStyle.height)
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(90, width / height, 0.1, 1000)
 
-scene.background = new THREE.Color(0xFFFFFF)
+scene.background = new THREE.Color(getComputedStyle(document.body).getPropertyValue("--main_color"))
 renderer.setSize(width, height)
 renderer.setPixelRatio(window.devicePixelRatio)
 
@@ -36,6 +36,52 @@ renderer.domElement.addEventListener('touchmove', onTouchMove)
 renderer.domElement.addEventListener('touchend', onTouchEnd)
 
 renderer.domElement.addEventListener('wheel', onMouseWheel)
+
+const darkMode = window.matchMedia('(prefers-color-scheme: dark)')
+let darkModeEnabled = darkMode.matches
+let wasClicked = false
+
+darkMode.addEventListener('change', function (e) {
+    if (e.matches && !darkModeEnabled) {
+        switchMode()
+    } else if(!e.matches && darkModeEnabled) {
+        switchMode()
+    }
+});
+
+const switchButton = document.getElementById("switch")
+if(darkModeEnabled) {
+    switchButton.src = "images/sun.svg"
+} else {
+    switchButton.src = "images/moon.svg"
+}
+switchButton.addEventListener('click', function(e) {
+    wasClicked = true
+    switchMode()
+})
+
+function switchMode() {
+    if(!darkModeEnabled) {
+        switchButton.src = "images/sun.svg"
+        darkModeEnabled = true
+    } else {
+        switchButton.src = "images/moon.svg"
+        darkModeEnabled = false
+    }
+
+    if(wasClicked) {
+        const bodyStyle = document.body.style
+        const computedStyle = getComputedStyle(document.body)
+
+        const main_color = computedStyle.getPropertyValue("--main_color")
+        const secondary_color = computedStyle.getPropertyValue("--secondary_color")
+
+        bodyStyle.setProperty("--secondary_color", main_color)
+        bodyStyle.setProperty("--main_color", secondary_color)
+
+        scene.background = new THREE.Color(secondary_color) // not main because we switched them
+    }
+}
 
 function onMouseDown(event) {
     renderer.domElement.style.cursor = "grabbing"
